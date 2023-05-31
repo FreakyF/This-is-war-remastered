76de6b477e317be45f948a57ae38b95af69b83e9
FreakyF <100375045+FreakyF@users.noreply.github.com>
2023-05-31 22:49:28 +0200

    Implement server and serverClient

--- src/main/java/pl/kielce/tu/drylofudala/system/Server.java
+++ src/main/java/pl/kielce/tu/drylofudala/system/Server.java
@@ -2,9 +2,14 @@ package pl.kielce.tu.drylofudala.system;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
@@ -13,31 +18,41 @@ public class Server {
     boolean running = true;
 
     public void start() throws IOException {
-
-        while (running) {
-            // Create server socket listening on port 8000
-            ServerSocket serverSocket = new ServerSocket(8000);
-            try (serverSocket) {
-                // Wait for a connection
-                Socket socket = serverSocket.accept();
-
-                // Create input and output streams to client
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
-                // Read message from client and send it back
-                String inputLine = in.readLine();
-                if (inputLine != null) {
-                    logger.debug("Input from client: {}", inputLine);
-                } else {
-                    logger.debug("Input from client is null!");
-                }
-                out.println("Input: " + inputLine);
-            } catch (IOException e) {
-                stop();
-                e.printStackTrace();
+        // Create server socket listening on port 8000
+        ServerSocket serverSocket = new ServerSocket(8000);
+        logger.debug("Server is listening on port: {}", serverSocket.getLocalPort());
+        try (serverSocket) {
+            while (running) {
+                handleClientConnection(serverSocket);
             }
         }
+    }
+
+    private void handleClientConnection(ServerSocket serverSocket) {
+        // Wait for a connection
+        try (Socket socket = serverSocket.accept()) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            readMessage(in, out);
+        } catch (IOException e) {
+            stop();
+            logger.error("An error occured during client connection: {}", e.getMessage(), e);
+            e.printStackTrace();
+        }
+    }
+
+    private static void readMessage(BufferedReader in, PrintWriter out) throws IOException {
+        // Read message from client and send it back
+        String inputLine = in.readLine();
+        if (inputLine != null) {
+            logger.debug("Input from client: {}", inputLine);
+            JSONObject jsonObject = new JSONObject(inputLine);
+            logger.info(jsonObject.getString("command"));
+        } else {
+            logger.debug("Input from client is null!");
+        }
+        out.println("Input: " + inputLine);
     }
 
     public void stop() {
--- src/main/java/pl/kielce/tu/drylofudala/system/ServerClient.java
+++ src/main/java/pl/kielce/tu/drylofudala/system/ServerClient.java
@@ -2,6 +2,8 @@ package pl.kielce.tu.drylofudala.system;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,40 +16,90 @@ public class ServerClient {
     private static final Logger logger = LogManager.getLogger(ServerClient.class);
 
     public static void main(String[] args) throws IOException {
-        CountDownLatch serverStartedLatch;
+        Result serverResult = startAndWaitForServer();
+
+        // Connect to server on localhost, port 8000
+        Socket firstClientSocket = new Socket("localhost", 8000);
+        logger.debug("First client connecting to server on port: {}", firstClientSocket.getLocalPort());
+        handleServerConnection(firstClientSocket, serverResult.clientsFinishedLatch());
+
+        Socket secondClientSocket = new Socket("localhost", 8000);
+        logger.debug("First client connecting to server on port: {}", secondClientSocket.getLocalPort());
+        handleServerConnection(secondClientSocket, serverResult.clientsFinishedLatch());
+
+        stopServerWithClientCompletion(serverResult.server(), serverResult.serverStoppedLatch(), serverResult.clientsFinishedLatch(), serverResult.serverThread());
+    }
+
+    @NotNull
+    private static Result startAndWaitForServer() {
         Server server = new Server();
-        serverStartedLatch = new CountDownLatch(1);
+        CountDownLatch serverStartedLatch = new CountDownLatch(1);
+        CountDownLatch serverStoppedLatch = new CountDownLatch(1);
+        CountDownLatch clientsFinishedLatch = new CountDownLatch(2);
 
         Thread serverThread = new Thread(() -> {
             try {
                 server.start();
                 serverStartedLatch.countDown();
-            } catch (IOException e) {
+                serverStoppedLatch.await();
+            } catch (IOException | InterruptedException e) {
+                logger.error("An error occurred while starting the server: ", e);
+                Thread.currentThread().interrupt();
                 e.printStackTrace();
             }
         });
         serverThread.start();
+        return new Result(server, serverStoppedLatch, clientsFinishedLatch, serverThread);
+    }
 
-        // Connect to server on localhost, port 8000
-        Socket socket = new Socket("localhost", 8000);
-        try (socket) {
-            // Create input and output streams to server
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            // Send a message to the server
-            out.println("Hello, Server!");
+    private record Result(Server server, CountDownLatch serverStoppedLatch, CountDownLatch clientsFinishedLatch, Thread serverThread) {
+    }
 
-            // Read the response from the server
-            String response = in.readLine();
-            if (response != null) {
-                logger.debug("Response from server: {}", response);
-            } else {
-                logger.debug("Response from server is null!");
-            }
-        } catch (IOException e) {
+    private static void stopServerWithClientCompletion(Server server, CountDownLatch serverStoppedLatch, CountDownLatch clientsFinishedLatch, Thread serverThread) {
+        serverStoppedLatch.countDown(); // Signal that the server has stopped
+        try {
+            clientsFinishedLatch.await(); // Wait for clients to finish before interrupting server thread
+        } catch (InterruptedException e) {
+            logger.error("An error occurred while starting the server: ", e);
+            Thread.currentThread().interrupt();
             e.printStackTrace();
         }
         server.stop();
         serverThread.interrupt();
+    }
+
+    private static void handleServerConnection(Socket socket, CountDownLatch clientsFinishedLatch) {
+        Thread clientThread = new Thread(() -> {
+            try (socket) {
+                // Create input and output streams to the client
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                sendMessage(out);
+                readMessage(in);
+            } catch (IOException e) {
+                logger.error("An error occurred while handling the server connection: ", e);
+                e.printStackTrace();
+            } finally {
+                clientsFinishedLatch.countDown();
+            }
+        });
+        clientThread.start();
+    }
+
+    private static void readMessage(BufferedReader in) throws IOException {
+        // Read the response from the server
+        String response = in.readLine();
+        if (response != null) {
+            logger.debug("Response from server: {}", response);
+        } else {
+            logger.debug("Response from server is null!");
+        }
+    }
+
+    private static void sendMessage(PrintWriter out) {
+        // Send a message to the server
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("command", "Hello");
+        out.println(jsonObject);
     }
 }
