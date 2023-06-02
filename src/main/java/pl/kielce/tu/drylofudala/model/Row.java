fd6e06c6701276802ab90c02871733f636a3a12c
Jan Urbaś <janurbas39@gmail.com>
2023-06-02 23:03:46 +0200

    Add getters, addCard and addCards methods to Row class.

--- src/main/java/pl/kielce/tu/drylofudala/model/Row.java
+++ src/main/java/pl/kielce/tu/drylofudala/model/Row.java
@@ -6,6 +6,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Row {
-    private PositionType type;
-    private List<Card> cards = new ArrayList<>();
+    private final PositionType type;
+    private final List<Card> cards = new ArrayList<>();
+
+    public Row(PositionType type) {
+        this.type = type;
+    }
+
+    public PositionType getType() {
+        return type;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public void addCards(List<Card> cards) {
+        this.cards.addAll(cards);
+    }
 }
