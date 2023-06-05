fa4f55b296b079fc610d6ed6c70af0a93623cf33
FreakyF <100375045+FreakyF@users.noreply.github.com>
2023-06-05 21:09:03 +0200

    #4: Partially implement register in AuthenticationService.

--- src/main/java/pl/kielce/tu/drylofudala/authentication/AuthenticationService.java
+++ src/main/java/pl/kielce/tu/drylofudala/authentication/AuthenticationService.java
@@ -1,119 +1,95 @@
 package pl.kielce.tu.drylofudala.authentication;
 
-import org.bouncycastle.crypto.generators.Argon2BytesGenerator;
-import org.bouncycastle.crypto.params.Argon2Parameters;
 import org.jetbrains.annotations.NotNull;
 import pl.kielce.tu.drylofudala.entity.Player;
-
-import java.security.SecureRandom;
+import pl.kielce.tu.drylofudala.persistance.IRepository;
 
 public class AuthenticationService implements IAuthenticationService {
+	private final IRepository<Player> playerRepository;
+	private final IHasher hasher;
 
-    @Override
-    public Player register(@NotNull String nickname, @NotNull String password) {
-        byte[] salt = generateSalt();
-        byte[] hashedPassword = hashPassword(password, salt);
-        // TODO: Implement adding user to the database;
-        return null;
-    }
+	public AuthenticationService(IRepository<Player> playerRepository, IHasher hasher) {
+		this.playerRepository = playerRepository;
+		this.hasher = hasher;
+	}
 
-    private byte[] hashPassword(String password, byte[] salt) {
-        Argon2Parameters parameters = new Argon2Parameters.Builder(Argon2Parameters.ARGON2_id)
-                .withSalt(salt)
-                .withIterations(AuthenticationConfig.ITERATIONS)
-                .withMemoryAsKB(AuthenticationConfig.MEMORY)
-                .build();
+	@Override
+	public Player register(@NotNull String nickname, @NotNull String password) {
+		byte[] salt = hasher.generateSalt();
+		String hashedPassword = hasher.hashPassword(password, salt);
+		Player player = new Player(nickname, hashedPassword, salt);
+		playerRepository.save(player);
+		return player;
+	}
 
-        Argon2BytesGenerator generator = new Argon2BytesGenerator();
-        generator.init(parameters);
+	@Override
+	public AuthenticationResult login(@NotNull String nickname, @NotNull String password) {
+		return null;
+	}
 
-        byte[] hashedPassword = new byte[AuthenticationConfig.HASHED_PASSWORD_LENGTH];
-        generator.generateBytes(password.getBytes(), hashedPassword);
+	@Override
+	public ValidationResult isPasswordValid(@NotNull String password) {
+		StringBuilder validationMessage = new StringBuilder();
+		if (isTooShort(password, AuthenticationConfig.MIN_PASSWORD_LENGTH)) {
+			validationMessage.append(ValidationMessage.PASSWORD_TOO_SHORT);
+			validationMessage.append('\n');
+		}
+		if (isTooLong(password, AuthenticationConfig.MAX_PASSWORD_LENGTH)) {
+			validationMessage.append(ValidationMessage.PASSWORD_TOO_LONG);
+			validationMessage.append('\n');
+		}
+		if (containsLowercase(password)) {
+			validationMessage.append(ValidationMessage.PASSWORD_WITHOUT_LOWERCASE);
+			validationMessage.append('\n');
+		}
+		if (containsUppercase(password)) {
+			validationMessage.append(ValidationMessage.PASSWORD_WITHOUT_UPPERCASE);
+			validationMessage.append('\n');
+		}
+		if (containsSpecialCharacters(password)) {
+			validationMessage.append(ValidationMessage.PASSWORD_WITHOUT_SPECIAL_CHARACTER);
+			validationMessage.append('\n');
+		}
+		if (containsNumber(password)) {
+			validationMessage.append(ValidationMessage.PASSWORD_WITHOUT_NUMBER);
+			validationMessage.append('\n');
+		}
 
-        return hashedPassword;
-    }
+		return !validationMessage.isEmpty() ? new ValidationResult(false, validationMessage.toString()) : new ValidationResult(true);
+	}
 
-    private byte[] generateSalt() {
-        byte[] salt = new byte[AuthenticationConfig.SALT_LENGTH];
-        SecureRandom secureRandom = new SecureRandom();
-        secureRandom.nextBytes(salt);
+	@Override
+	public ValidationResult isNicknameValid(@NotNull String nickname) {
+		StringBuilder validationMessage = new StringBuilder();
+		if (isTooShort(nickname, AuthenticationConfig.MIN_NICKNAME_LENGTH)) {
+			validationMessage.append(ValidationMessage.NICKNAME_TOO_SHORT);
+		} else if (isTooLong(nickname, AuthenticationConfig.MAX_NICKNAME_LENGTH)) {
+			validationMessage.append(ValidationMessage.NICKNAME_TOO_LONG);
+		}
+		return !validationMessage.isEmpty() ? new ValidationResult(false, validationMessage.toString()) : new ValidationResult(true);
+	}
 
-        return salt;
-    }
+	private boolean isTooShort(final String text, final int minLength) {
+		return text.length() < minLength;
+	}
 
-    @Override
-    public AuthenticationResult login(@NotNull String nickname, @NotNull String password) {
-        // TODO: Implement login method.
-        return null;
-    }
+	private boolean isTooLong(final String text, final int maxLength) {
+		return text.length() > maxLength;
+	}
 
-    @Override
-    public ValidationResult isPasswordValid(@NotNull String password) {
-        StringBuilder validationMessage = new StringBuilder();
-        if (isTooShort(password, AuthenticationConfig.MIN_PASSWORD_LENGTH)) {
-            validationMessage.append(ValidationMessage.PASSWORD_TOO_SHORT);
-            validationMessage.append('\n');
-        }
-        if (isTooLong(password, AuthenticationConfig.MAX_PASSWORD_LENGTH)) {
-            validationMessage.append(ValidationMessage.PASSWORD_TOO_LONG);
-            validationMessage.append('\n');
-        }
-        if (containsLowercase(password)) {
-            validationMessage.append(ValidationMessage.PASSWORD_WITHOUT_LOWERCASE);
-            validationMessage.append('\n');
-        }
-        if (containsUppercase(password)) {
-            validationMessage.append(ValidationMessage.PASSWORD_WITHOUT_UPPERCASE);
-            validationMessage.append('\n');
-        }
-        if (containsSpecialCharacters(password)) {
-            validationMessage.append(ValidationMessage.PASSWORD_WITHOUT_SPECIAL_CHARACTER);
-            validationMessage.append('\n');
-        }
-        if (containsNumber(password)) {
-            validationMessage.append(ValidationMessage.PASSWORD_WITHOUT_NUMBER);
-            validationMessage.append('\n');
-        }
+	private boolean containsLowercase(final String password) {
+		return password.matches(".*[a-z].*");
+	}
 
-        return !validationMessage.isEmpty()
-                ? new ValidationResult(false, validationMessage.toString())
-                : new ValidationResult(true);
-    }
+	private boolean containsUppercase(final String password) {
+		return password.matches(".*[A-Z].*");
+	}
 
-    @Override
-    public ValidationResult isNicknameValid(@NotNull String nickname) {
-        StringBuilder validationMessage = new StringBuilder();
-        if (isTooShort(nickname, AuthenticationConfig.MIN_NICKNAME_LENGTH)) {
-            validationMessage.append(ValidationMessage.NICKNAME_TOO_SHORT);
-        } else if (isTooLong(nickname, AuthenticationConfig.MAX_NICKNAME_LENGTH)) {
-            validationMessage.append(ValidationMessage.NICKNAME_TOO_LONG);
-        }
-        return !validationMessage.isEmpty()
-                ? new ValidationResult(false, validationMessage.toString())
-                : new ValidationResult(true);
-    }
+	private boolean containsSpecialCharacters(final String password) {
+		return password.matches(".*[^a-zA-Z0-9].*");
+	}
 
-    private boolean isTooShort(final String text, int minLength) {
-        return text.length() < minLength;
-    }
-
-    private boolean isTooLong(final String text, int maxLength) {
-        return text.length() > maxLength;
-    }
-
-    private boolean containsLowercase(final String password) {
-        return password.matches(".*[a-z].*");
-    }
-
-    private boolean containsUppercase(final String password) {
-        return password.matches(".*[A-Z].*");
-    }
-
-    private boolean containsSpecialCharacters(final String password) {
-        return password.matches(".*[^a-zA-Z0-9].*");
-    }
-
-    private boolean containsNumber(final String password) {
-        return password.matches(".*\\d.*");
-    }
+	private boolean containsNumber(final String password) {
+		return password.matches(".*\\d.*");
+	}
 }
