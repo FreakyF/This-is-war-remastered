06ca532d3a57a9ed1d94fe9fae4aa9e5e30b3111
FreakyF <100375045+FreakyF@users.noreply.github.com>
2023-06-05 17:25:48 +0200

    #4: Add password salt type to player entity and add access annotation.

--- src/main/java/pl/kielce/tu/drylofudala/entity/Player.java
+++ src/main/java/pl/kielce/tu/drylofudala/entity/Player.java
@@ -1,42 +1,56 @@
 package pl.kielce.tu.drylofudala.entity;
 
+import jakarta.persistence.Access;
+import jakarta.persistence.AccessType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 @Entity
 @Table(name = "player")
+@Access(AccessType.FIELD)
 public class Player extends BaseEntity {
-    private String name;
-    private String hashedPassword;
+	private String name;
+	private String hashedPassword;
 
-    protected Player() {
-    }
+	private byte[] passwordSalt;
 
-    public Player(String name, String hashedPassword) {
-        this.name = name;
-        this.hashedPassword = hashedPassword;
-    }
+	protected Player() {
+	}
 
-    public String getName() {
-        return name;
-    }
+	public Player(String name, String hashedPassword, byte[] passwordSalt) {
+		this.name = name;
+		this.hashedPassword = hashedPassword;
+		this.passwordSalt = passwordSalt;
+	}
 
-    public String getHashedPassword() {
-        return hashedPassword;
-    }
+	public String getName() {
+		return name;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Player player = (Player) o;
-        return Objects.equals(getName(), player.getName()) && Objects.equals(getHashedPassword(), player.getHashedPassword());
-    }
+	public String getHashedPassword() {
+		return hashedPassword;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getName(), getHashedPassword());
-    }
+	public byte[] getPasswordSalt() {
+		return passwordSalt;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		if (!super.equals(o)) return false;
+		Player player = (Player) o;
+		return Objects.equals(name, player.name) && Objects.equals(hashedPassword, player.hashedPassword) && Arrays.equals(passwordSalt, player.passwordSalt);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = Objects.hash(super.hashCode(), name, hashedPassword);
+		result = 31 * result + Arrays.hashCode(passwordSalt);
+		return result;
+	}
 }
