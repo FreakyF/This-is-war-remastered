aa8afd94d3c1fb44d4b495f4308fac34bab1b169
FreakyF <100375045+FreakyF@users.noreply.github.com>
2023-06-06 21:12:18 +0200

    #4: Partially implement StartMenu and implement ImagePanel and UiConfig.

---
+++ src/main/java/pl/kielce/tu/drylofudala/ui/ImagePanel.java
@@ -0,0 +1,19 @@
+package pl.kielce.tu.drylofudala.ui;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Image;
+
+public class ImagePanel extends JPanel {
+	protected final transient Image backgroundImage;
+
+	public ImagePanel(Image backgroundImage) {
+		this.backgroundImage = backgroundImage;
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(backgroundImage, 0, 0, this);
+	}
+}
---
+++ src/main/java/pl/kielce/tu/drylofudala/ui/UiConfig.java
@@ -0,0 +1,15 @@
+package pl.kielce.tu.drylofudala.ui;
+
+import java.awt.Font;
+
+public class UiConfig {
+	public static final String FONT_FAMILY = "JetBrains Mono";
+	public static final Font COPYRIGHT_FONT = new Font(FONT_FAMILY, Font.PLAIN, 24);
+
+	public static final Font TITLE_FONT = new Font(FONT_FAMILY, Font.PLAIN, 144);
+
+	public static final Font BUTTON_FONT = new Font(FONT_FAMILY, Font.PLAIN, 56);
+
+	private UiConfig() {
+	}
+}
