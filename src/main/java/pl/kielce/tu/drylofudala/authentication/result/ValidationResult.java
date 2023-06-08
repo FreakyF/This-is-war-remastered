08b418287972d33c84118e86435397822abd6977
Jan Urbaś <janurbas39@gmail.com>
2023-06-08 12:17:33 +0200

    #4: Move validation messages to properties file.

--- src/main/java/pl/kielce/tu/drylofudala/authentication/result/ValidationResult.java
+++ src/main/java/pl/kielce/tu/drylofudala/authentication/result/ValidationResult.java
@@ -1,17 +1,38 @@
 package pl.kielce.tu.drylofudala.authentication.result;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Properties;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import pl.kielce.tu.drylofudala.authentication.AuthenticationConfig;
 
 public record ValidationResult(@NotNull boolean valid, @Nullable List<String> messages) {
-	public static final String PASSWORD_TOO_SHORT = "Password is too short. Minimum length is: " + AuthenticationConfig.MIN_PASSWORD_LENGTH;
-	public static final String PASSWORD_TOO_LONG = "Password is too long. Maximum length is: " + AuthenticationConfig.MAX_PASSWORD_LENGTH;
-	public static final String PASSWORD_WITHOUT_LOWERCASE = "Password does not contains at least one lowercase character";
-	public static final String PASSWORD_WITHOUT_UPPERCASE = "Password does not contains at least one uppercase character";
-	public static final String PASSWORD_WITHOUT_SPECIAL_CHARACTER = "Password does not contains at least one special character";
-	public static final String PASSWORD_WITHOUT_NUMBER = "Password does not contains at least one number character";
-	public static final String NICKNAME_TOO_SHORT = "Nickname is too short. Minimum length is: " + AuthenticationConfig.MIN_NICKNAME_LENGTH;
-	public static final String NICKNAME_TOO_LONG = "Nickname is too long. Maximum length is: " + AuthenticationConfig.MAX_NICKNAME_LENGTH;
+	public static final String PASSWORD_TOO_SHORT;
+	public static final String PASSWORD_TOO_LONG;
+	public static final String PASSWORD_WITHOUT_LOWERCASE;
+	public static final String PASSWORD_WITHOUT_UPPERCASE;
+	public static final String PASSWORD_WITHOUT_SPECIAL_CHARACTER;
+	public static final String PASSWORD_WITHOUT_NUMBER;
+	public static final String NICKNAME_TOO_SHORT;
+	public static final String NICKNAME_TOO_LONG;
+
+	static {
+		Properties properties = new Properties();
+		try (InputStream inputStream = ValidationResult.class.getResourceAsStream("/validationMessages.properties")) {
+			properties.load(inputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		PASSWORD_TOO_SHORT = String.format(properties.getProperty("PASSWORD_TOO_SHORT"), AuthenticationConfig.MIN_PASSWORD_LENGTH);
+		PASSWORD_TOO_LONG = String.format(properties.getProperty("PASSWORD_TOO_LONG"), AuthenticationConfig.MAX_PASSWORD_LENGTH);
+		PASSWORD_WITHOUT_LOWERCASE = properties.getProperty("PASSWORD_WITHOUT_LOWERCASE");
+		PASSWORD_WITHOUT_UPPERCASE = properties.getProperty("PASSWORD_WITHOUT_UPPERCASE");
+		PASSWORD_WITHOUT_SPECIAL_CHARACTER = properties.getProperty("PASSWORD_WITHOUT_SPECIAL_CHARACTER");
+		PASSWORD_WITHOUT_NUMBER = properties.getProperty("PASSWORD_WITHOUT_NUMBER");
+		NICKNAME_TOO_SHORT = String.format(properties.getProperty("NICKNAME_TOO_SHORT"), AuthenticationConfig.MIN_NICKNAME_LENGTH);
+		NICKNAME_TOO_LONG = String.format(properties.getProperty("NICKNAME_TOO_LONG"), AuthenticationConfig.MAX_NICKNAME_LENGTH);
+	}
 }
