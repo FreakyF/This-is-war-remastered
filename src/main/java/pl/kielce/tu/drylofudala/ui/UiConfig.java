e8e2f2ff196021b610d2e6a2d4c840285493e6bf
FreakyF <100375045+FreakyF@users.noreply.github.com>
2023-06-10 23:10:36 +0200

    #4: Change font type to CallOfOpsDuty

--- src/main/java/pl/kielce/tu/drylofudala/ui/UiConfig.java
+++ src/main/java/pl/kielce/tu/drylofudala/ui/UiConfig.java
@@ -1,10 +1,20 @@
 package pl.kielce.tu.drylofudala.ui;
 
+import pl.kielce.tu.drylofudala.persistance.resource.ResourceRepository;
+
 import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
 import java.awt.Insets;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class UiConfig {
-	public static final String FONT_FAMILY = "Comic Sans MS";
+	Font customFont;
+	public static final String FONT_FAMILY = "Call Of Ops Duty";
 	public static final Font COPYRIGHT_FONT = new Font(FONT_FAMILY, Font.PLAIN, 24);
 
 	public static final Font TITLE_FONT = new Font(FONT_FAMILY, Font.PLAIN, 72);
@@ -13,5 +23,23 @@ public class UiConfig {
 	public static final Insets GBC_INSETS = new Insets(10, 10, 10, 10);
 
 	private UiConfig() {
+	}
+
+	static {
+		Font customFont;
+		try {
+			URI x = ResourceRepository.getInstance().getResourceFromPath("CallOfOpsDuty.otf").toURI();
+			customFont = Font.createFont(Font.TRUETYPE_FONT, new File(x));
+
+		} catch (FontFormatException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
+
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		ge.registerFont(customFont);
 	}
 }
