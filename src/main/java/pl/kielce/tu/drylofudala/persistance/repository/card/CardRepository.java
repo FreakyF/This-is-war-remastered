0e0c69d5c951ccb563ce10ef756c7f2213d1fb71
FreakyF <100375045+FreakyF@users.noreply.github.com>
2023-06-16 21:57:11 +0200

    #2: Implement CardRepository

---
+++ src/main/java/pl/kielce/tu/drylofudala/persistance/repository/card/CardRepository.java
@@ -0,0 +1,10 @@
+package pl.kielce.tu.drylofudala.persistance.repository.card;
+
+import pl.kielce.tu.drylofudala.entity.Card;
+import pl.kielce.tu.drylofudala.persistance.dbcontext.DbContext;
+
+public class CardRepository extends DbContext<Card> implements ICardRepository {
+	public CardRepository(final Class<Card> entityClass) {
+		super(entityClass);
+	}
+}
---
+++ src/main/java/pl/kielce/tu/drylofudala/persistance/repository/card/ICardRepository.java
@@ -0,0 +1,7 @@
+package pl.kielce.tu.drylofudala.persistance.repository.card;
+
+import pl.kielce.tu.drylofudala.entity.Card;
+import pl.kielce.tu.drylofudala.persistance.dbcontext.IDbContext;
+
+public interface ICardRepository extends IDbContext<Card> {
+}
